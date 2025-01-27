7fdbd9b26a0f02ff124257fbae0b0107768f24f9
Jiu <99115192+JiuGeFaCai@users.noreply.github.com>
2025-01-27 18:30:14 +0800

    Update api_proxy.h

--- rwProcMem33Module/rwProcMem_module/api_proxy.h
+++ rwProcMem33Module/rwProcMem_module/api_proxy.h
@@ -1,11 +1,7 @@
 ﻿#ifndef API_PROXY_H_
 #define API_PROXY_H_
 #include "ver_control.h"
-#include "linux_kernel_api/linux_kernel_api_4_19_81.h"
-#include "linux_kernel_api/linux_kernel_api_5_4_61.h"
-#include "linux_kernel_api/linux_kernel_api_5_10_43.h"
-#include "linux_kernel_api/linux_kernel_api_5_15_41.h"
-#include "linux_kernel_api/linux_kernel_api_6_1_75.h"
+#include "linux_kernel_api/linux_kernel_api.h"
 #include <linux/ctype.h>
 #include <asm/uaccess.h>
 #include <linux/uaccess.h>
